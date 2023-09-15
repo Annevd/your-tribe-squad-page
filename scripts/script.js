e1a739ff8121ab084be1c148f785fd4e7fb75080
rileyesther <144009597+rileyesther@users.noreply.github.com>
2023-09-15 11:52:32 +0200

    Merge branch 'main' of https://github.com/rileyesther/your-tribe-squad-page into main

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,75 @@
-//hier komt js
+document.addEventListener('DOMContentLoaded', function () {
+    const imageContainers = document.querySelectorAll('.image-container');
+    let containerId = null;
+    const overlay = document.getElementById('overlay');
+
+
+    function removeFadeInClass() {
+        imageContainers.forEach(container => {
+            container.classList.remove('fade-in');
+        });
+    }
+
+    setTimeout(removeFadeInClass, 1000);
+
+    imageContainers.forEach(container => {
+        container.addEventListener('click', () => {
+            overlay.style.zIndex = '5';
+
+            containerId = container.id;
+
+            switch (containerId) {
+                
+                case 'Q2':
+                    container.style.transform = 'rotate(15deg) scale(1.85) translate(0%, -10%)';
+                    break;
+                case 'Q3':
+                    container.style.transform = 'rotate(-15deg) scale(1.85) translate(0%, -10%)';
+                    break;
+                
+                default:
+                    break;
+            }
+
+
+            container.querySelector('img').style.opacity = '0';
+
+            container.style.zIndex = '10';
+
+            
+
+            if (containerId === 'Q2') {
+                container.querySelector('.q2-text').style.display = 'block';
+            }
+
+            if (containerId === 'Q3') {
+                container.querySelector('.q3-text').style.display = 'block';
+            }
+
+            
+        });
+
+        container.addEventListener('mouseout', () => {
+            overlay.style.zIndex = '-1';
+
+            container.querySelector('img').style.opacity = '1';
+
+            container.style.transform = 'none';
+
+            container.style.zIndex = '1';
+
+            
+
+            if (containerId === 'Q2') {
+                container.querySelector('.q2-text').style.display = 'none';
+            }
+
+            if (containerId === 'Q3') {
+                container.querySelector('.q3-text').style.display = 'none';
+            }
+
+            
+        });
+    });
+});
+
